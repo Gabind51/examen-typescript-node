efaf2e972c42179bd09594f01b010135f2e4b1f6
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2024-03-05 16:45:15 +0100

    6. Modification du persons.json et adaptation du type person

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,19 @@ import { readFileSync } from "fs";
 
 type person = {
   age: number;
-  height: number
+  taille?: number;
+  height?: number;
 }
 function getStatistics() : { meanAge: number, meanHeight: number } {
   const persons : person[] = JSON.parse(readFileSync("./persons.json").toString());
   const totalAges : number = persons.reduce((sum, person) => sum + person.age, 0)
-  const totalHeights: number = persons.reduce((sum, person) => sum + person.height, 0)
+  const totalHeights: number = persons.reduce((sum, person) => sum + (person.height || person.taille || 0), 0)
   const meanAge: number = totalAges / persons.length
   const meanHeight: number = totalHeights / persons.length
 
   return {
-    meanAge, meanHeight}
+    meanAge, meanHeight
+  }
 }
 
 function displayResult() {
