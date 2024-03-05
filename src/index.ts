cc8aa2b9c64ca2b9fbca3f48977e9349a24ab530
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2024-03-05 16:52:31 +0100

    7. Vérification du format de persons.json et affichage d'un message d'erreur si le format n'est pas correct

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,18 @@ type person = {
   taille?: number;
   height?: number;
 }
-function getStatistics() : { meanAge: number, meanHeight: number } {
+
+function checkPersonsStructure(person: person) : boolean {
+  return person.taille !== undefined || person.height !== undefined
+}
+function getStatistics() : { meanAge: number, meanHeight: number } | void {
   const persons : person[] = JSON.parse(readFileSync("./persons.json").toString());
+
+  if(!persons.every(checkPersonsStructure)) {
+    console.error('Les données du fichier persons.json ne sont pas correctement formatées.')
+    return
+  }
+
   const totalAges : number = persons.reduce((sum, person) => sum + person.age, 0)
   const totalHeights: number = persons.reduce((sum, person) => sum + (person.height || person.taille || 0), 0)
   const meanAge: number = totalAges / persons.length
