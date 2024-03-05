5720eb0f6fcb0bb7abbe032333bb6c6e9faac905
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2024-03-05 16:25:07 +0100

    4. getStatistiques retourne un objet avec meanAge et meanHeight

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ type person = {
   age: number;
   height: number
 }
-function getStatistics() : number {
+function getStatistics() : { meanAge: number, meanHeight: number } {
   const persons : person[] = JSON.parse(readFileSync("./persons.json").toString());
   return persons.reduce((max, person) => (person.age > max ? person.age : max), persons[0].age)
 }
